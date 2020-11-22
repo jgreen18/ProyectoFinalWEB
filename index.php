5bb823505e61517a8ebdf97e43d02f0daf0706e9
Javier <jgreen_18@alu.uabcs.mx>
2020-11-21 18:48:42 -0700

    carpeta2

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>netfix</title>
+</head>
+<body>
+
+	<div>
+		<form>
+			<fieldset>
+
+				<img src="imagenes/usuario.png">
+				<legend>iniciar sesion</legend>
+			<input type="text" name="name" placeholder="USERNAME">
+			<p><a href="">olvidaste tu usuario ?</a></p>
+			<input type="password" name="pass" placeholder="* * * * * * * ">
+			<p><a href="">olvidaste tu contraseña ?</a></p>
+
+
+			</fieldset>
+		</form>
+	</div>
+
+</body>
+</html>

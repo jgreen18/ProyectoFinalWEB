b4a4249b028c130d354762fb71142ff3d8fef529
Javier <jgreen_18@alu.uabcs.mx>
2020-11-24 12:19:31 -0700

    avance 24/nov

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Getfix</title>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/index.css?v0.0.8">
+</head>
+<body style=" " >
+	
+					
+		
+			<div class="is">
+				<form>
+					
+					<div class="cir">
+						<img src="assets/imagenes/chico.png" >
+						
+					</div>
+						
+					<input type="text" name="name" placeholder="USERNAME" height="29px">
+					<p><a href="">olvidaste tu usuario ?</a></p>
+					<input type="password" name="pass" placeholder="* * * * * * * ">
+					<p><a href="#" >olvidaste tu contraseña ?</a></p>
+
+					<button id="boton"  >Iniciar sesion</button>
+
+
+				</form>
+			
+		</div>
+	
+<!-- <script type="text/javascript">
+	function muestraOculta(obj,p_id) {
+
+            var p = document.getElementById(p_id);
+
+                 if (p.style.display != "none") { 
+
+                    p.style.display="none";
+
+                    
+
+                    
+
+                 } else{
+                    p.style.display="block";
+
+                    
+
+                 }
+
+            }
+	
+</script> -->
+</body>
+</html>

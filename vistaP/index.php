cc349b9876d6f6282e9aaef748e42806ff353032
Parvelcin <65757617+Parvelcin@users.noreply.github.com>
2020-12-09 07:02:41 -0700

    vista de reproduccion terminada

---
+++ vistaP/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		
+	</title>
+</head>
+<body style="margin: 0; padding: 0; background-color: #5D6D7E; color: white;">
+
+	<div style="width: 100%; height: 30px; background-color: black; padding-top: 1%;">
+		<h3 style="color: orange; font-family: arial; margin: 0; margin-left: 2%;"><a href="" style="text-decoration: none; color: orange;">Getfix</a></h3>
+	</div>
+
+	<div style="width: 75%; height: 428px; display: inline-block; margin: 0; padding-top: 4.6%;">
+		<iframe width="100%" height="100%" src="https://www.youtube.com/embed/Ja0Trjgay2w" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</div>
+
+	<div style=" width: 24.69%; height: 500px; display: inline-block; margin: 0; vertical-align: top;">
+		<h3 style="font-family: arial; margin-left: 2%; color: #DC7633;">Peliculas recomendadas:</h3>
+		<p style="font-size: 12px; font-family: arial;">
+			<a href=""><img src="../assets/imagenes/kickass.jpg" width="40%" style="float: left; margin-right: 2%"></a><b>KICK-ASS:</b> A pesar de la falta completa de poderes extraordinarios, un adolescente se reinventa a sí mismo como superhéroe y se enfrenta a un criminal. 
+		</p>
+
+		<p style="font-size: 12px; font-family: arial;">
+			<a href=""><img src="../assets/imagenes/Daredevil.jpg" width="40%" style="float: left; margin-right: 2%"></a><b>DAREDEVIL:</b> Ciego desde que era joven, Matt Murdock lucha contra la injusticia de día como abogado y por la noche como Daredevil en Nueva York. 
+		</p>
+		
+		<p style="font-size: 12px; font-family: arial;">
+			<a href=""><img src="../assets/imagenes/creed.jpg" width="40%" style="float: left; margin-right: 2%"></a><b>CREED:</b> El excampeón del mundo de peso pesado Rocky Balboa se desempeña como el entrenador y mentor de Adonis Johnson, el hijo de su difunto amigo Apollo Creed. 
+		</p>
+
+		<p style="font-size: 12px; font-family: arial;">
+			<a href=""><img src="../assets/imagenes/ted.jpg" width="40%" style="float: left; margin-right: 2%"></a><b>TED:</b> Cuando John Bennett era un niño pequeño, pidió el deseo de que Ted, su querido oso de peluche, cobrara vida. pero este oso terminó siendo un oso maloso muy grosero.
+		</p>
+
+		<p style="font-size: 12px; font-family: arial;">
+			<a href=""><img src="../assets/imagenes/vecinos.jpg" width="40%" style="float: left; margin-right: 2%"></a><b>BUENOS VECINOS:</b> Mac y Kelly acaban de tener una bebé adorable y se han comprado una preciosa casa. Los problemas comienzan cuando sus nuevos vecinos son una fraternidad.
+
+	</div>
+
+	<div style="background-color: #1B2631; width: 75%; height: 80px; padding-top: 2%;">
+		<img style="margin-left: 2%; display: inline-block;" src="../assets/imagenes/ver.png">
+		<p style="margin-left: 0.5%; margin-top: 0; margin-right: 3%; display: inline-block;"> 0</p>
+		
+		<a href=""><div style="display: inline-block;">
+			<img id="likes" style="margin-left: 2%; display: inline-block;" src="../assets/imagenes/likes.png">
+		</div></a>
+		<p style="margin-left: 0.5%; margin-top: 0; margin-right: 3%; display: inline-block;"> 0</p>
+	</div>
+</body>
+</html>
